0a673924abe43e7a74e69d8859a5b6f568b62ba4
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2019-01-28 18:55:09 -0500

    Added switch statement for success and debugging failures

--- enhancer.js
+++ enhancer.js
@@ -4,14 +4,74 @@ module.exports = {
   repair
 };
 
+function enhancedCheck(item) {
+  let upgrade = item.enhancement;
+  upgrade += 1;
+  console.log(upgrade);
+  if (upgrade > 20) {
+    throw new Error("item");
+  } else {
+    // item.enhancement += 1;
+    let name = item.origName;
+    switch (upgrade) {
+      case 1:
+      case 2:
+      case 3:
+      case 4:
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 10:
+      case 11:
+      case 12:
+      case 13:
+      case 14:
+      case 15:
+        item.name = `[+${upgrade}] ${name}`;
+        break;
+      case 16:
+        item.name = `[PRI] ${name}`;
+        break;
+      case 17:
+        item.name = `[DUO] ${name}`;
+        break;
+      case 18:
+        item.name = `[TRI] ${name}`;
+        break;
+      case 19:
+        item.name = `[TET] ${name}`;
+        break;
+      case 20:
+        item.name = `[PEN] ${name}`;
+        break;
+    }
+  }
+}
+
 function success(item) {
-  return null;
+  if (!item) return null;
+  if (item.type !== "armour" && item.type !== "weapon") {
+    return console.error({ error: "can enhance weapon and armour only" });
+  } else {
+    enhancedCheck(item);
+    console.log(item);
+    return {
+      origName: item.origName,
+      name: `${item.name}`,
+      type: item.type,
+      durability: item.durability,
+      enhancement: item.enhancement + 1
+    };
+  }
 }
 
 function fail(item) {
-  return null;
+  return item;
 }
 
 function repair(item) {
-  return null;
+  item.durability = 100;
+  return item;
 }
--- enhancer.spec.js
+++ enhancer.spec.js
@@ -1,19 +1,39 @@
 const enhanced = require("./enhancer");
 
+const item = {
+  origName: "Lambda Axe",
+  name: "[+8] Lambda Axe",
+  type: "armour",
+  durability: 90,
+  enhancement: 20
+};
+const itemE = {
+  origName: "Lambda Axe",
+  name: "[+21] Lambda Axe",
+  type: "armour",
+  durability: 90,
+  enhancement: 21
+};
+
 describe("enhancement library", () => {
   describe("success() method", () => {
     test("should take in an obj and return an enhanced obj", () => {
-      expect(enhanced.succeed()).toBe(null);
+      expect(enhanced.success(item)).toEqual(itemE);
+    });
+    test("should throw error when item exceeds twenty", () => {
+      expect(() => {
+        enhanced.success(itemE);
+      }).toThrow();
     });
   });
-  describe("fail() method", () => {
-    test("should take in an object and return a object with failed enhancer", () => {
-      expect(enhanced.fail()).toBe(null);
-    });
-  });
-  describe("repair() method", () => {
-    test("should take in an obj and return a obj with failed enhancer", () => {
-      expect(enhanced.repair()).toBe(null);
-    });
-  });
+  //   describe("fail() method", () => {
+  //     test("should take in an object and return a object with failed enhancer", () => {
+  //       expect(enhanced.fail()).toBe(null);
+  //     });
+  //   });
+  //   describe("repair() method", () => {
+  //     test("should take in an obj and return a obj with failed enhancer", () => {
+  //       expect(enhanced.repair()).toBe(null);
+  //     });
+  //   });
 });
